13f5dcc8db953963c944a7a7a3bf369ff5215e9d
nighthawk-ops <mmmkzera@gmail.com>
2025-05-11 20:59:09 +0300

    ui added

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+// Array storing books
+const myLibrary=[];
+
 // Book constructor
-function Book(title, author, pages, read){
+// yor = year-of-release
+function Book(title, author, pages, yor){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = function(){
-        if(read == "yes"){
-            return "read";
-        }
-        else{
-            return "not read yet";
-        }
-    };
-    this.info = function(){
-        return this.title +" by "+ this.author + " , " + this.pages+" pages , "+this.read();
-    };
+    this.yor = yor;
+    // this.read = function(){
+    //     if(read == "yes"){
+    //         return "read";
+    //     }
+    //     else{
+    //         return "not read yet";
+    //     }
+    // };
+    // this.info = function(){
+    //     return this.title +" by "+ this.author + " , " + this.pages+" pages , "+this.read();
+    // };
 }
 
-const Hobit = new Book("Hobit", "J.R.R Tolkien", 295, "yes");
-console.log(Hobit.title);
-console.log(Hobit.author);
-console.log(Hobit.pages);
-console.log(Hobit.read());
-console.log(Hobit.info());
+function addBookToLibrary(title, author, pages, yor){
+    const book = new Book(title, author, pages, yor);
+    book.id = crypto.randomUUID();
+   myLibrary.push(book);
+}
